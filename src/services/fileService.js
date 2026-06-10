14cb87fee82a0befe6a711ffb688cf603cc4606a
Angel Pauly <angelpauly72@gmail.com>
2026-06-10 18:40:36 +0530

    retrieve all the subfolders

--- src/services/fileService.js
+++ src/services/fileService.js
@@ -1,8 +1,34 @@
 import fs from "fs/promises";
 import path from "path";
-export async function readRepositoryFiles(repoPath) {
-    const items = await fs.readdir(repoPath);
 
-    return items;
+export async function readRepositoryFiles(dirPath) {
 
+    let allFiles = [];
+
+    const items = await fs.readdir(dirPath, {
+        withFileTypes: true
+    });
+
+    for (const item of items) {
+
+        const fullPath = path.join(
+            dirPath,
+            item.name
+        );
+
+        if (item.isDirectory()) {
+
+            const nestedFiles =
+                await readRepositoryFiles(fullPath);
+
+            allFiles.push(...nestedFiles);
+
+        } else {
+
+            allFiles.push(fullPath);
+
+        }
+    }
+
+    return allFiles;
 }
